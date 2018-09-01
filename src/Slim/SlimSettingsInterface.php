e4a97a7bffc0b7e080b6338f3ee3ea0954ebf442
Dominik Zogg <dominik.zogg@gmail.com>
2018-09-01 15:54:23 +0200

    do not extend the config interface

--- src/Slim/SlimSettingsInterface.php
+++ src/Slim/SlimSettingsInterface.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace Chubbyphp\Config\Slim;
 
-use Chubbyphp\Config\ConfigInterface;
-
-interface SlimSettingsInterface extends ConfigInterface
+interface SlimSettingsInterface
 {
     /**
      * @return array
